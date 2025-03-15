b9090e2ba4f67fb863436e4a17dbe924b64da7b4
Manux17 <st10965@iismarconipieralisi.it>
2025-03-15 08:31:05 +0100

    Creazione Progetto

---
+++ ChiCercaTrova/ChiCercaTrova/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChiCercaTrova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
