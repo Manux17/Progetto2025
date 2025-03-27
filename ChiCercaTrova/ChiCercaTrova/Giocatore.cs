50b0e615307515e53082c772670fe0844aaf7ed4
Manux17 <st10965@iismarconipieralisi.it>
2025-03-27 12:00:02 +0100

    Creazione Classi

---
+++ ChiCercaTrova/ChiCercaTrova/Giocatore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChiCercaTrova
+{
+    class Giocatore
+    {
+        public List<int> Mazzo;
+
+        public void CreaMazzo()
+        {
+
+        }
+
+
+    }
+}
